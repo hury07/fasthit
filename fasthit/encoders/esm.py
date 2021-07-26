2e8a435ee445b2f22f3f9952e8e5101b909e55eb
Ruyun Hu <hury07@hotmail.com>
2021-07-26 14:26:23 +0800

    Rewrite "pretrained" for esm

--- fasthit/encoders/esm.py
+++ fasthit/encoders/esm.py
@@ -4,6 +4,13 @@ import numpy as np
 import pandas as pd
 import torch
 from typing import Sequence, Tuple, Union
+###
+from argparse import Namespace
+import warnings
+import urllib
+from pathlib import Path
+import esm
+###
 
 import fasthit
 
@@ -46,9 +53,8 @@ class ESM(fasthit.Encoder):
         self._target_python_idxs = target_python_idxs
         self._embeddings = {}
         
-        from esm import pretrained
         self._device = torch.device('cuda:0' if torch.cuda.is_available() and not nogpu else 'cpu')
-        pretrained_model, esm_alphabet = pretrained.load_model_and_alphabet(
+        pretrained_model, esm_alphabet = Pretrained.load_model_and_alphabet(
             pretrained_model_dir + self._encoding["model"]+".pt"
         )
         self._pretrained_model = pretrained_model.eval().to(self._device)
@@ -211,3 +217,135 @@ class ESM(fasthit.Encoder):
                 results.append(self._embed(msas))
         shutil.rmtree(self._temp_dir)
         return np.concatenate(results, axis=0)
+
+
+class Pretrained():
+    # Copyright (c) Facebook, Inc. and its affiliates.
+    #
+    # This source code is licensed under the MIT license found in the
+    # LICENSE file in the root directory of this source tree.
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def load_model_and_alphabet(cls, model_name):
+        if model_name.endswith(".pt"):  # treat as filepath
+            return cls.load_model_and_alphabet_local(model_name)
+        else:
+            return cls.load_model_and_alphabet_hub(model_name)
+
+    @classmethod
+    def load_model_and_alphabet_local(cls, model_location):
+        """ Load from local path. The regression weights need to be co-located """
+        model_location = Path(model_location)
+        model_data = torch.load(model_location, map_location="cpu")
+        regression_data = None
+        return cls.load_model_and_alphabet_core(model_data, regression_data)
+    
+    @classmethod
+    def load_model_and_alphabet_hub(cls, model_name):
+        url = f"https://dl.fbaipublicfiles.com/fair-esm/models/{model_name}.pt"
+        model_data = cls.load_hub_workaround(url)
+        regression_data = None
+        return cls.load_model_and_alphabet_core(model_data, regression_data)
+
+    @classmethod
+    def load_hub_workaround(cls, url):
+        try:
+            data = torch.hub.load_state_dict_from_url(url, progress=False, map_location="cpu")
+        except RuntimeError:
+            # Pytorch version issue - see https://github.com/pytorch/pytorch/issues/43106
+            fn = Path(url).name
+            data = torch.load(
+                f"{torch.hub.get_dir()}/checkpoints/{fn}",
+                map_location="cpu",
+            )
+        except urllib.error.HTTPError as e:
+            raise Exception(f"Could not load {url}, check if you specified a correct model name?")
+        return data
+
+    @classmethod
+    def has_emb_layer_norm_before(cls, model_state):
+        """ Determine whether layer norm needs to be applied before the encoder """
+        return any(k.startswith("emb_layer_norm_before") for k, param in model_state.items())
+
+    @classmethod
+    def load_model_and_alphabet_core(cls, model_data, regression_data=None):
+        if regression_data is not None:
+            model_data["model"].update(regression_data["model"])
+
+        alphabet = esm.Alphabet.from_architecture(model_data["args"].arch)
+
+        if model_data["args"].arch == "roberta_large":
+            # upgrade state dict
+            pra = lambda s: "".join(s.split("encoder_")[1:] if "encoder" in s else s)
+            prs1 = lambda s: "".join(s.split("encoder.")[1:] if "encoder" in s else s)
+            prs2 = lambda s: "".join(
+                s.split("sentence_encoder.")[1:] if "sentence_encoder" in s else s
+            )
+            model_args = {pra(arg[0]): arg[1] for arg in vars(model_data["args"]).items()}
+            model_state = {prs1(prs2(arg[0])): arg[1] for arg in model_data["model"].items()}
+            model_state["embed_tokens.weight"][alphabet.mask_idx].zero_()  # For token drop
+            model_args["emb_layer_norm_before"] = cls.has_emb_layer_norm_before(model_state)
+            model_type = esm.ProteinBertModel
+
+        elif model_data["args"].arch == "protein_bert_base":
+
+            # upgrade state dict
+            pra = lambda s: "".join(s.split("decoder_")[1:] if "decoder" in s else s)
+            prs = lambda s: "".join(s.split("decoder.")[1:] if "decoder" in s else s)
+            model_args = {pra(arg[0]): arg[1] for arg in vars(model_data["args"]).items()}
+            model_state = {prs(arg[0]): arg[1] for arg in model_data["model"].items()}
+            model_type = esm.ProteinBertModel
+        elif model_data["args"].arch == "msa_transformer":
+
+            # upgrade state dict
+            pra = lambda s: "".join(s.split("encoder_")[1:] if "encoder" in s else s)
+            prs1 = lambda s: "".join(s.split("encoder.")[1:] if "encoder" in s else s)
+            prs2 = lambda s: "".join(
+                s.split("sentence_encoder.")[1:] if "sentence_encoder" in s else s
+            )
+            prs3 = lambda s: s.replace("row", "column") if "row" in s else s.replace("column", "row")
+            model_args = {pra(arg[0]): arg[1] for arg in vars(model_data["args"]).items()}
+            model_state = {prs1(prs2(prs3(arg[0]))): arg[1] for arg in model_data["model"].items()}
+            if model_args.get("embed_positions_msa", False):
+                emb_dim = model_state["msa_position_embedding"].size(-1)
+                model_args["embed_positions_msa_dim"] = emb_dim  # initial release, bug: emb_dim==1
+
+            model_type = esm.MSATransformer
+
+        else:
+            raise ValueError("Unknown architecture selected")
+
+        model = model_type(
+            Namespace(**model_args),
+            alphabet,
+        )
+
+        expected_keys = set(model.state_dict().keys())
+        found_keys = set(model_state.keys())
+
+        if regression_data is None:
+            expected_missing = {"contact_head.regression.weight", "contact_head.regression.bias"}
+            error_msgs = []
+            missing = (expected_keys - found_keys) - expected_missing
+            if missing:
+                error_msgs.append(f"Missing key(s) in state_dict: {missing}.")
+            unexpected = found_keys - expected_keys
+            if unexpected:
+                error_msgs.append(f"Unexpected key(s) in state_dict: {unexpected}.")
+
+            if error_msgs:
+                raise RuntimeError(
+                    "Error(s) in loading state_dict for {}:\n\t{}".format(
+                        model.__class__.__name__, "\n\t".join(error_msgs)
+                    )
+                )
+            if expected_missing - found_keys:
+                warnings.warn(
+                    "Regression weights not found, predicting contacts will not produce correct results."
+                )
+
+        model.load_state_dict(model_state, strict=regression_data is not None)
+
+        return model, alphabet
